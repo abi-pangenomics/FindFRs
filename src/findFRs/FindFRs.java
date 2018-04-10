9a8760b5bad1bc84ac4f89edd2cac31ebb716c2a
Brendan Mumey <brendan.mumey@montana.edu>
2018-04-10 12:02:50 -0600

    commented out gff parsing skeleton - decided to filter on gene content as a post process

--- src/findFRs/FindFRs.java
+++ src/findFRs/FindFRs.java
@@ -10,21 +10,21 @@ import java.util.concurrent.atomic.*;
 import java.io.*;
 import java.util.stream.IntStream;
 import org.apache.commons.cli.*;
-import org.biojava.nbio.genome.parsers.gff.*;
+//import org.biojava.nbio.genome.parsers.gff.*;
 
 public class FindFRs {
 
     // parameters:
     static String dotFile = ""; // .dot filename
     static String fastaFile = ""; // .fasta filename
-    static String gffFile = ""; // .gff filename
+    //static String gffFile = ""; // .gff filename
     static int K = -1; // k-mer size
     static double alpha = 0.7; // epsilon_r parameter
     static int kappa = 0; // maxInsert parameter
     static int minSup = 1;
     static int minLen = 1;
     static boolean useRC = false; // indicates if fasta file was appended with its reverse-complement
-    static double minGC = 0.0;
+    //static double minGC = 0.0;
 
     static Graph g;
     static ArrayList<Sequence> sequences;
@@ -38,7 +38,7 @@ public class FindFRs {
     static ConcurrentHashMap<Integer, ClusterNode> nodeCluster;
     static int numClusterNodes = 0;
 
-    static FeatureList gffFeatures;
+//    static FeatureList gffFeatures;
 
     static String[] colors = {"122,39,25", "92,227,60", "225,70,233", "100,198,222", "232,176,49", "50,39,85",
         "67,101,33", "222,142,186", "92,119,227", "206,225,151", "227,44,118", "229,66,41",
@@ -100,14 +100,14 @@ public class FindFRs {
 
         sequences = ReadInput.readFastaFile(fastaFile);
 
-        if (gffFile != "") {
-            GFF3Reader gffReader = new GFF3Reader();
-            try {
-                gffFeatures = gffReader.read(gffFile);
-            } catch (Exception ex) {
-                System.out.println("problem reading gff file: " + ex);
-            }
-        }
+//        if (gffFile != "") {
+//            GFF3Reader gffReader = new GFF3Reader();
+//            try {
+//                gffFeatures = gffReader.read(gffFile);
+//            } catch (Exception ex) {
+//                System.out.println("problem reading gff file: " + ex);
+//            }
+//        }
 
     }
 
@@ -680,13 +680,13 @@ public class FindFRs {
         rcO.setRequired(false);
         options.addOption(rcO);
 
-        Option gffO = new Option("g", "gff", true, "gff file");
-        gffO.setRequired(false);
-        options.addOption(gffO);
-
-        Option gcO = new Option("c", "mingc", true, "min gene content fraction");
-        gcO.setRequired(false);
-        options.addOption(gcO);
+//        Option gffO = new Option("g", "gff", true, "gff file");
+//        gffO.setRequired(false);
+//        options.addOption(gffO);
+//
+//        Option gcO = new Option("c", "mingc", true, "min gene content fraction");
+//        gcO.setRequired(false);
+//        options.addOption(gcO);
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -717,13 +717,13 @@ public class FindFRs {
 
         useRC = cmd.hasOption("rc");
 
-        if (cmd.hasOption("gff")) {
-            gffFile = cmd.getOptionValue("gff");
-        }
-
-        if (cmd.hasOption("mingc")) {
-            minGC = Double.parseDouble(cmd.getOptionValue("mingc"));
-        }
+//        if (cmd.hasOption("gff")) {
+//            gffFile = cmd.getOptionValue("gff");
+//        }
+//
+//        if (cmd.hasOption("mingc")) {
+//            minGC = Double.parseDouble(cmd.getOptionValue("mingc"));
+//        }
 
         readData();
         buildPaths();
